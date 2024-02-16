c52bc7d499a6947a8b737cace3e0e67e9a6cbb4a
Crowdbotics <team@crowdbotics.com>
2024-02-16 21:51:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "musica-sierra-46430",
+  url: "https://musica-sierra-46430.botics.co",
+  api: "https://musica-sierra-46430.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
